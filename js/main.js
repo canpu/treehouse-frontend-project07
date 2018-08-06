ce0a1cbe5abcddc60fd5179edbda93a2423b63e7
Can Pu <pucanmail@gmail.com>
2018-08-06 00:08:30 -0500

    Modified the rest from flex layout to grid layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const animationTime = 500;
 const $alertSlide = $(".alert__slide");
+const $alertIcon = $(".icon__alert");
 
 $alertSlide.click(function () {
   $alertSlide.slideUp(animationTime);
+});
+
+$alertIcon.click(function () {
+  $alertSlide.slideDown(animationTime);
 });
