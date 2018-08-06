9be35813aeda849a900e5335cebbdad128414705
Can Pu <pucanmail@gmail.com>
2018-08-06 00:39:15 -0500

    Added the hint messages when hovering over navagation icons

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const animationTime = 500;
 const $alertSlide = $(".alert__slide");
 const $alertIcon = $(".icon__alert");
 
+/* Displaying and Hidding the Alert Message */
 $alertSlide.click(function () {
   $alertSlide.slideUp(animationTime);
 });
